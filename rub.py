088c51d238bdea3e9d022f06137611b1bb2644a5
EaeDave <daveisrd@gmail.com>
2023-10-20 12:56:29 -0300

    Function Apply filter - Stock bigger then 0 and send supplier code

--- rub.py
+++ rub.py
@@ -1,19 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 # RUB IP
 IP = 'IP HERE'
+
+# RUB User Credential
+USER = 'USER HERE'
+
+# RUB Password Credential
+PASSWORD = 'PASSWORD HERE'
+
+# Function to wait load process (Basicly wait 1.5s when it's called)
+def WAIT_LOAD():
+    time.sleep(1.5)
+
+supplier_code = input('Digite o código do fornecedor: ')
 
 # Accessing product page from Rub Website
 DRIVER = webdriver.Chrome()
 DRIVER.get(f'http:{IP}/vue/#/core/op/produto')
 
 
-# Function that login into the Web Server
+# Function that login into the Website
 def LoginRub():
-    USER = 'USER HERE'
-    PASSWORD = 'PASSWORD HERE'
-
     USER_BOX = DRIVER.find_element(By.ID , 'login-fld-usr')
     USER_BOX.send_keys(USER)
 
@@ -22,3 +32,46 @@ def LoginRub():
 
     LOGIN_BUTTON = DRIVER.find_element(By.ID, 'login-vbtn-loginbtn')
     LOGIN_BUTTON.click()
+
+
+def ApplyFilter():
+    # Click on filter button
+    FILTER_BUTTON = DRIVER.find_element(By.XPATH, '//div[@class="pull-left botoes-table listOptions"]')
+    FILTER_BUTTON.click()
+
+
+    # Click on stock filter
+    SELECT_STOCK_FILTER = DRIVER.find_element(By.XPATH, '//option[@value="ESTOQUE_DISPONIVEL"]')
+    SELECT_STOCK_FILTER.click()
+
+
+    # Select bigger then filter from stock
+    BIGGER_THEN_FILTER = DRIVER.find_element(By.XPATH, '//option[@value="3"]')
+    BIGGER_THEN_FILTER.click()
+
+
+    # Send bigger then 0
+    VALUE_FROM_STOCK = DRIVER.find_element(By.XPATH, '//input[@placeholder="Valor"]')
+    VALUE_FROM_STOCK.send_keys('0')
+
+
+    # Click on supplier filter
+    SELECT_SUPPLIER_FILTER = DRIVER.find_element(By.XPATH, '//option[@value="T3_NOME"]')
+    SELECT_SUPPLIER_FILTER.click()
+
+
+    # Send supplier code to supplier filter
+    VALUE_FROM_SUPPLIER = DRIVER.find_element(By.XPATH, '(//input[@placeholder="Valor"])[2]')
+    VALUE_FROM_SUPPLIER.send_keys(supplier_code)
+
+    # Click on apply filter button
+    APPLY_FILTER_BUTTON = DRIVER.find_element(By.XPATH, '//a[@class="fwrub btn btn-primary btnApply"]')
+    APPLY_FILTER_BUTTON.click()
+
+
+LoginRub()
+WAIT_LOAD()
+
+ApplyFilter()
+WAIT_LOAD()
+
