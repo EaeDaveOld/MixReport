df0d9fff4649130ad3dea6aea7d664717eb9f10b
EaeDave <daveisrd@gmail.com>
2023-10-20 11:29:22 -0300

    Login Rub Function

--- rub.py
+++ rub.py
@@ -7,3 +7,18 @@ IP = 'IP HERE'
 # Accessing product page from Rub Website
 DRIVER = webdriver.Chrome()
 DRIVER.get(f'http:{IP}/vue/#/core/op/produto')
+
+
+# Function that login into the Web Server
+def LoginRub():
+    USER = 'USER HERE'
+    PASSWORD = 'PASSWORD HERE'
+
+    USER_BOX = DRIVER.find_element(By.ID , 'login-fld-usr')
+    USER_BOX.send_keys(USER)
+
+    PASSWORD_BOX = DRIVER.find_element(By.ID, 'login-fld-pwd')
+    PASSWORD_BOX.send_keys(PASSWORD)
+
+    LOGIN_BUTTON = DRIVER.find_element(By.ID, 'login-vbtn-loginbtn')
+    LOGIN_BUTTON.click()
