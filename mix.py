efd544a84a61f1092a64f092567f16ab626b19aa
EaeDave <daveisrd@gmail.com>
2023-10-21 10:53:48 -0300

    Try and Except supplier code + GerenatePDF Function

--- mix.py
+++ mix.py
@@ -11,15 +11,31 @@ USER = 'USER HERE'
 # RUB Password Credential
 PASSWORD = 'PASSWORD HERE'
 
-# Function to wait load process (Basicly wait 1.5s when it's called)
-def WAIT_LOAD():
-    time.sleep(1.5)
+# Don't change this supplier code
+supplier_code = None
 
-supplier_code = input('Digite o código do fornecedor: ')
+
+# Function to request supplier_code
+def Request_SupplierCode():
+    global supplier_code
+    Stop_Loop = False
+    while Stop_Loop is False:
+        try:
+            supplier_code = int(input('Digite o código do fornecedor: '))
+            Stop_Loop = True
+        except ValueError:
+            print('Digite somente números inteiros e sem espaços em branco.')
+
+
+Request_SupplierCode()
 
 # Accessing product page from Rub Website
 DRIVER = webdriver.Chrome()
 DRIVER.get(f'http:{IP}/vue/#/core/op/produto')
+
+# Function to wait load process (Basicly wait 0.5s when it's called)
+def Wait_Load():
+    time.sleep(1)
 
 
 # Function that login into the Website
@@ -34,9 +50,10 @@ def LoginRub():
     LOGIN_BUTTON.click()
 
 
+# Function that apply filter bigger then zero and supplier code
 def ApplyFilter():
     # Click on filter button
-    FILTER_BUTTON = DRIVER.find_element(By.XPATH, '//div[@class="pull-left botoes-table listOptions"]')
+    FILTER_BUTTON = DRIVER.find_element(By.XPATH, '//*[@id="master-vbtn-optionsdialogopenbutton"]')
     FILTER_BUTTON.click()
 
 
@@ -52,6 +69,7 @@ def ApplyFilter():
 
     # Send bigger then 0
     VALUE_FROM_STOCK = DRIVER.find_element(By.XPATH, '//input[@placeholder="Valor"]')
+    VALUE_FROM_STOCK.clear()
     VALUE_FROM_STOCK.send_keys('0')
 
 
@@ -62,6 +80,7 @@ def ApplyFilter():
 
     # Send supplier code to supplier filter
     VALUE_FROM_SUPPLIER = DRIVER.find_element(By.XPATH, '(//input[@placeholder="Valor"])[2]')
+    VALUE_FROM_SUPPLIER.clear()
     VALUE_FROM_SUPPLIER.send_keys(supplier_code)
 
     # Click on apply filter button
@@ -69,9 +88,35 @@ def ApplyFilter():
     APPLY_FILTER_BUTTON.click()
 
 
+# Function that generate report PDF
+def GeneratePDF():
+    GENERATE_PDF_BUTTON = DRIVER.find_element(By.XPATH, '//*[@id="mainview"]/div[1]/div/div[1]/div/div[3]/div[3]/ul[2]/li[3]/a')
+    GENERATE_PDF_BUTTON.click()
+
+
+# Function to close PDF report Window
+def Close_PDF_Window():
+    PDF_Window = DRIVER.window_handles[1]
+    DRIVER.switch_to.window(PDF_Window)
+    DRIVER.close()
+
+
+# Function to select de first window
+def Select_First_Window():
+    Actual_Window = DRIVER.window_handles[0]
+    DRIVER.switch_to.window(Actual_Window)
+
+
 LoginRub()
-WAIT_LOAD()
+Wait_Load()
 
 ApplyFilter()
-WAIT_LOAD()
+Wait_Load()
+
+GeneratePDF()
+Wait_Load()
+
+input()
+
+
 
